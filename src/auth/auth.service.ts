7537da74c9cbe8a544e950b5c30b8d2dcba40d7c
Vikas Kushwaha <vikaskushwaha@Vikass-MacBook-Air.local>
2024-08-13 01:29:10 +0530

    add signup login in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-// import * as argon from 'argon2';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-    signup(dto: AuthDto) {
-        console.log(dto);
-        //generate the hashed password
+    async signup(dto: AuthDto) {
+        try {
+            //generate the hashed password
+            const hash = await argon.hash(dto.password);
 
-        //create and save the user in db
+            //create and save the user in db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
 
-        //return the saved user
-        return { msg: 'I have signed up' };
+            delete user.hash; //delete password from the db
+
+            //return the saved user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                throw new ForbiddenException('Credentials taken');
+            }
+            throw error;
+        }
     }
     signin() {
         return { msg: 'I have signed in' };
