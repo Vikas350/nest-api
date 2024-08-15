c9a96ad22f7f7e39843cbbe98befc69fa9f607ed
Vikas Kushwaha <vikaskushwaha@Vikass-MacBook-Air.local>
2024-08-16 01:21:35 +0530

    add scripts for migration & config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-    imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+    imports: [
+        ConfigModule.forRoot({
+             isGlobal: true, // so that we can use config service globally, otherwise throw error
+        }), //used to load the env variables in module
+        AuthModule,
+        UserModule,
+        BookmarkModule,
+        PrismaModule,
+    ],
 })
 export class AppModule {}
